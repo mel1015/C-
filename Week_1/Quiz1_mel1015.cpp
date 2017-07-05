7ceba678779705bf29f6a0ddd1513c9fbfc907f6
신광식 <sks1015sks@naver.com>
2017-07-05 12:36:51 +0900

    [신광식] Week_1 / Quiz_1

---
+++ Week_1/Quiz1_mel1015.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() 
+{
+	char name[21];
+	char cpy_name[21];
+
+	cout << "허가된 사용자 이름을 입력하세요." << endl;
+
+	while(true)
+	{
+		cout << "이름 : ";
+		cin.getline(name, 21);
+		strcpy_s(cpy_name, strlen(name) + 1, name);
+
+		if (!strcmp(cpy_name, "leo jeong")) {
+			cout << "허가된 사용자가 맞습니다." << endl;
+			cout << "복사된 사용자의 이름 : " << cpy_name << endl;
+			cout << "사용자 이름의 길이 : " << strlen(cpy_name) << endl;
+			break;
+		}
+		else
+			cout << "허가된 사용자가 아닙니다" << endl;
+	}
+
+	return 0;
+}
