1001edbf1b3af258cb27cc08b059859d5188483b
신광식 <sks1015sks@naver.com>
2017-07-05 14:54:55 +0900

    [신광식] Week_3 / Quiz_1,2

---
+++ Week_5/Quiz1_mel1015.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+char* shiftStr(char* pstr, int psize, int prepeat);
+
+int main()
+{
+	char* str = "Running Group Study!!!";
+
+	shiftStr(str, 22, 22);
+
+	return 0;
+}
+
+char* shiftStr(char* pstr, int psize, int prepeat)
+{
+	char* str = nullptr;
+	char* strArray;
+	char temp;
+
+	str = pstr;
+	cout << str << endl;
+
+	strArray = new char[psize + 1];
+	strcpy_s(strArray, psize + 1, str);
+
+	temp = strArray[0];
+
+	for (int i = 0; i < psize; i++)
+	{
+		strArray[i] = strArray[i + 1];
+	}
+	strArray[psize - 1] = temp;
+
+	if (prepeat == 0)
+		return 0;
+	else
+		return shiftStr(strArray, 22, prepeat - 1);
+}
---
+++ Week_5/Quiz2_mel1015.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string reverseStr(string pstr);
+
+int main()
+{
+	string str = "";
+
+	while (1)
+	{
+		cout << "영어 문자열을 입력<0은 종료> : ";
+		getline(cin, str);
+		if (str.compare("0") == 0)
+			break;
+		else
+		{
+			cout << "뒤집힌 문자열 : " << reverseStr(str)
+				<< endl << endl;
+		}
+	}
+	return 0;
+}
+
+string reverseStr(string pstr)
+{
+	char* strArray;
+	char temp;
+	string str;
+
+	strArray = new char[pstr.size() + 1];
+	strcpy_s(strArray, pstr.size() + 1, pstr.c_str());
+
+	for (int i = 0; i < (signed)pstr.size() / 2; i++)
+	{
+		temp = strArray[i];
+		strArray[i] = strArray[pstr.size() - (i + 1)];
+		strArray[pstr.size() - (i + 1)] = temp;
+	}
+	str = strArray;
+
+	return str;
+}
