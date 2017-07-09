bcbefa6b2a0fef2d25730ca4c09fb088555832d1
신광식 <sks1015sks@naver.com>
2017-07-09 17:05:07 +0900

    [신광식] Week_8 / Quiz_1

---
+++ Week_8/Employee_mel1015.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+class Employee
+{
+public:
+	Employee();
+	Employee(char* name, int number, int pay);
+	~Employee();
+
+	char* getName();
+	void setName(char* name);
+	int getNumber();
+	void setNumber(int number);
+	int getPay();
+	void setPay(int pay);
+	void showDetail();
+
+private:
+	char* name;
+	int number;
+	int pay;
+};
